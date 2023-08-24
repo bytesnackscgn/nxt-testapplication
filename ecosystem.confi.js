d5fde4d11a5aa75ed490abdaa6df3732196816b7
Georgij Michel <bytesnackscgn@gmail.com>
2023-08-24 05:51:31 +0200

    pm2

---
+++ ecosystem.confi.js
@@ -0,0 +1,24 @@
+module.exports = {
+	apps: [
+		{
+			name: 'nxt-testapplication',
+			port: 3000,
+			instances: '1',
+			exec_mode: 'fork',
+			script: './dist/index.js',
+			env: {
+				CONFIG_PATH: '/home/nxt-testapplication/.env.developement',
+				NODE_ENV: 'developement',
+			},
+			autorestart: true,
+			watch: false,
+			max_old_space_size: '2048M',
+			max_memory_restart: '1800M',
+			out_file: './pm2-log/out.log',
+			error_file: './pm2-log/error.log',
+			log_file: './pm2-log/log.log',
+			log_type: 'json',
+			log_date_format: 'DD-MM-YYYY hh:mm',
+		},
+	],
+};
